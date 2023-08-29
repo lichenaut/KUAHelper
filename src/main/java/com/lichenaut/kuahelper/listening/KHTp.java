92fa52ed522a277fa37fa8aaa920282c2f649dd0
BuildTools <unconfigured@null.spigotmc.org>
2023-08-29 15:54:04 -0600

    Now, all player teleportation types are checked for minimum distance.

---
+++ src/main/java/com/lichenaut/kuahelper/listening/KHTp.java
@@ -0,0 +1,32 @@
+package com.lichenaut.kuahelper.listening;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
+import net.luckperms.api.util.Tristate;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class KHTp implements Listener {
+
+    private final LuckPerms lp;
+
+    public KHTp(LuckPerms lp) {this.lp = lp;}
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onTp(PlayerTeleportEvent e) {
+        if (!e.getCause().equals(PlayerTeleportEvent.TeleportCause.COMMAND)) return;
+        Player p = e.getPlayer();
+        User user = lp.getUserManager().getUser(p.getName());
+        if (user == null || user.getCachedData().getPermissionData(QueryOptions.defaultContextualOptions()).checkPermission("kua.tp.bypass") == Tristate.TRUE) return;
+        Location from = e.getFrom(), to = e.getTo();
+        if (from.getWorld().equals(to.getWorld()) && from.distance(to) < 2000) {
+            e.setCancelled(true);
+            p.sendMessage("§7Teleportation cancelled, as it covers a distance less than two thousand blocks.");
+        }
+    }
+}
